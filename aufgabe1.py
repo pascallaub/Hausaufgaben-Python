9d523fab7cc44a5f3eed3728334b6e226dc39c59
Pascal <nutritionexpertition@gmail.com>
2024-10-01 14:11:41 +0200

    Rename aufgabe1.py to Aufgabe1.py

--- aufgabe1.py
+++
@@ -1,7 +0,0 @@
-#Definieren der Variablen
-first_name = "Pascal"
-last_name = "Laub"
-birth_year = 1991
-has_license = True
-
-print(f"Name:", first_name, last_name,"\nGeboren im Jahr:", birth_year,"\nHat einen Führerschein?", has_license)
